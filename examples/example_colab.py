12af8d7b64378a529fcfd48cf62857d4f24b56d5
lannguyen <18120051@student.hcmus.edu.vn>
2022-05-04 11:34:56 +0700

    run example on colab

---
+++ examples/example_colab.py
@@ -0,0 +1,27 @@
+import cv2
+import facealignment
+from google.colab.patches import cv2_imshow
+
+
+# Read sample images
+single_face = cv2.imread("tests/samples/single_face.png")
+multi_face = cv2.imread("tests/samples/multi_face.png")
+
+# Instantiate FaceAlignmentTools class
+tool = facealignment.FaceAlignmentTools()
+
+# MTCNN need RGB instead of CV2-BGR images
+single_face = cv2.cvtColor(single_face, cv2.COLOR_BGR2RGB)
+multi_face = cv2.cvtColor(multi_face, cv2.COLOR_BGR2RGB)
+
+# Align image with single face
+aligned_img = tool.align(single_face)
+screen_img = cv2.hconcat([single_face, aligned_img])
+screen_img = cv2.cvtColor(screen_img, cv2.COLOR_RGB2BGR)
+cv2_imshow(screen_img)
+
+# Align image with multiple faces
+aligned_imgs = tool.align(multi_face, allow_multiface=True)
+screen_img = cv2.hconcat([multi_face] + aligned_imgs)
+screen_img = cv2.cvtColor(screen_img, cv2.COLOR_RGB2BGR)
+cv2_imshow(screen_img)
